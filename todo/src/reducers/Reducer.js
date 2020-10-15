79a03f4519af4c1d4777bbd3687d59d653496aeb
Heather Beckman <heatherbeckman85@gmail.com>
2020-10-14 22:29:51 -0600

    hooked todolist up with reducer

--- todo/src/reducers/Reducer.js
+++ todo/src/reducers/Reducer.js
@@ -1,12 +1,17 @@
 
+export const TODO_ITEM = "TODO_ITEM";
+
 export const initialState = {
     item: 'Learn about reducers',
     completed: false,
     id: 3892987589
   };
 
-export const Reducer = (state, action) {
+export const Reducer = (state, action) => {
     switch (action.type) {
+        case TODO_ITEM:
+            return {...state, item: action.payload, completed: false,
+            id: Date.now()};
         default:
             return state;
     }
